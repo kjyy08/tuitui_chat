c307c5db5b97265b92f386b2f1bacea80d4d49c3
김주엽 <kjyy08@naver.com>
2024-09-30 00:14:57 +0900

    24.09.30 create project

---
+++ src/main/java/suftware/tuitui/TuituiChatApplication.java
@@ -0,0 +1,13 @@
+package suftware.tuitui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuituiChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuituiChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/suftware/tuitui/TuituiChatApplicationTests.java
@@ -0,0 +1,13 @@
+package suftware.tuitui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuituiChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
