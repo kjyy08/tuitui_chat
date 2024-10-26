7f77c0cd7dab82bc770395e8d7cc3268f8e6d877
김주엽 <kjyy08@naver.com>
2024-10-26 20:01:26 +0900

    24.10.25 빌드 테스트

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -17,13 +17,17 @@ source ./health_check.sh
 main() {
     log "배포 시작일자 : $(date +'%Y-%m-%d %H:%M:%S')"
 
-    EXIST_BLUE=$(docker-compose -p "${DOCKER_APP_NAME}-blue" -f docker-compose.blue.yml ps | grep -E "Up|running")
+    # blue-green 배포 시도했으나 ec2 인스턴스 하나로 api, 채팅용 서버, redis를 가동하는건 무리가 가서 실패..ㅜ
+    # git push 하면 자동 빌드되게 blue만 사용하자.
+    ./deploy_blue.sh
 
-    if [ -z "$EXIST_BLUE" ]; then
-        ./deploy_blue.sh  # 블루 배포
-    else
-        ./deploy_green.sh  # 그린 배포
-    fi
+    # EXIST_BLUE=$(docker-compose -p "${DOCKER_APP_NAME}-blue" -f docker-compose.blue.yml ps | grep -E "Up|running")
+
+    # if [ -z "$EXIST_BLUE" ]; then
+    #     ./deploy_blue.sh  # 블루 배포
+    # else
+    #     ./deploy_green.sh  # 그린 배포
+    # fi
 
     # 사용하지 않는 이미지 삭제
     docker image prune -af
