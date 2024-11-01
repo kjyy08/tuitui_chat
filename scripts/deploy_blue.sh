f3848882509b92a8f8fda66eb394ecd7e8277f7f
김주엽 <kjyy08@naver.com>
2024-11-01 18:05:42 +0900

    24.11.01 무중단 배포 테스트

--- scripts/deploy_blue.sh
+++ scripts/deploy_blue.sh
@@ -18,14 +18,14 @@ deploy_blue() {
     log "blue 배포 시작 : $(date +'%Y-%m-%d %H:%M:%S')"
     docker-compose -p "${DOCKER_APP_NAME}-blue" -f docker-compose.blue.yml up -d --build
 
-    if check_health 8443; then
-        log "green 중단 시작 : $(date +'%Y-%m-%d %H:%M:%S')"
-        docker-compose -p "${DOCKER_APP_NAME}-green" -f docker-compose.green.yml down
-    else
-        log "블루 배포가 실패하여 롤백합니다."
-        docker-compose -p "${DOCKER_APP_NAME}-blue" -f docker-compose.blue.yml down
-        exit 1
-    fi
+    #if check_health 8443; then
+    #    log "green 중단 시작 : $(date +'%Y-%m-%d %H:%M:%S')"
+    #    docker-compose -p "${DOCKER_APP_NAME}-green" -f docker-compose.green.yml down
+    #else
+    #    log "블루 배포가 실패하여 롤백합니다."
+    #    docker-compose -p "${DOCKER_APP_NAME}-blue" -f docker-compose.blue.yml down
+    #    exit 1
+    #fi
 }
 
 # 스크립트 실행
